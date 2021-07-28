fee4b2a76bb1cf9c25b5103ad5d4d88f5e993732
Aaron Schiff <aaron@schiff.nz>
2021-07-28 20:22:01 +1200

    started inflation forecast

---
+++ src/inflation.R
@@ -0,0 +1,10 @@
+# Forecasts of New Zealand's annual CPI inflation rate
+
+# Created by aaron@schiff.nz
+# https://github.com/aaronschiff/honest-economic-forecasts 
+
+# Data updated to: 2021 Q2
+
+# *****************************************************************************
+
+
