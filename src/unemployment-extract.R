963d4afb40a8da8395c6e82b85e0f7248107891b
Aaron Schiff <aaron@schiff.nz>
2021-07-29 16:41:19 +1200

    added unemployment data

---
+++ src/unemployment-extract.R
@@ -0,0 +1,17 @@
+# Extract unemployment rate data from giant Stats NZ labour market stats file
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+dat <- read_csv(file = here("data/unemployment/raw/2021Q1/unemployment.csv"), 
+                col_types = "ccncciccccccc") |> 
+  clean_names()
+
+unemp <- dat |> 
+  filter(group == "Labour Force Status by Sex: Seasonally Adjusted") |> 
+  filter(series_title_1 == "Unemployment Rate") |> 
+  filter(series_title_2 == "Total Both Sexes") 
+
+write_csv(x = unemp, 
+          file = here("data/unemployment/2021Q1/unemployment.csv"))
